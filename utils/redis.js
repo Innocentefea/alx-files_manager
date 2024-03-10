d71a3ecbd320c3426a13091add4580da9db14536
kamaufnjeri <kamaufnjeri2019@gmail..com>
2024-03-10 16:13:44 +0300

    Finished task zero class to connect to redis chect if connected, get a value using key, set value with espiration time and del with key

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,42 @@
-import { createRedis } from 'redis';
-import { promisify } from 'utils';
-
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor () {
-        // connect to redis
-        this.client = createRedis();
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setexAsync = promisify(this.client.setex).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
+  constructor() {
+    // connect to redis
+    this.client = createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
 
-        // incase of an error
-        this.client.on('error', (error) => {
-            console.log(`Error connecting to redis: ${error}`);
-        })
+    // incase of an error
+    this.client.on('error', (error) => {
+      console.log(`Error connecting to redis: ${error}`);
+    });
 
-        // on connections
-        this.client.on('connect', () => {
-            console.log('Redis server connected');
-        })
-    }
+    // on connections
+    this.client.on('connect', () => {
+      console.log('Redis server connected');
+    });
+  }
 
-    // return true if connection is arrive
-    isAlive = () => {
-        return this.client.connected;
-    }
+  // return true if connection is arrive
+  isAlive() {
+    return this.client.connected;
+  }
 
-    get = async(key) => {
-        const value = await this.getAsync(key);
-        return value;
-    }
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
 
-    set = async(key, value, duration) => {
-        await this.setexAsync(key, duration, value);
-    }
+  async set(key, value, duration) {
+    await this.setexAsync(key, duration, value);
+  }
 
-    del = async(key) => {
-        await this.delAsync(key);
-    }
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 module.exports = new RedisClient();
