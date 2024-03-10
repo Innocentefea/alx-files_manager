deeef3aacfe772ac3da06833a9e45e112009d508
kamaufnjeri <kamaufnjeri2019@gmail..com>
2024-03-10 19:00:31 +0300

    made updates to db.js and created controllers for app and the api routes

--- utils/db.js
+++ utils/db.js
@@ -5,31 +5,20 @@ class DBClient {
     // create url and connect to mongodb database
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
     this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.isConnected = false;
-    this.db = null;
-  }
-
-  // connect to mongodb
-  async connect() {
-    await this.client.connect();
-    this.isConnected = true;
-    this.db = this.client.db(this.database);
-    this.isConnected = this.client.isConnected();
+    this.client.connect();
+    this.db = this.client.db(database);
   }
 
   // return true if connected to mongodb
-  async isAlive() {
-    await this.connect();
-    const status = this.client.isConnected();
-    return status;
+  isAlive() {
+    return this.client.isConnected();
   }
 
   // number of users in db
   async nbUsers() {
-    await this.connect();
     const userCount = await this.db.collection('users').countDocuments();
     if (userCount) {
       return userCount;
@@ -39,7 +28,6 @@ class DBClient {
 
   // number of files in db
   async nbFiles() {
-    await this.connect();
     const fileCount = await this.db.collection('files').countDocuments();
 
     if (fileCount) {
