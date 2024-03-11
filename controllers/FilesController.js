313c3d7de2187071524233d5e472c1947bbd326f
kamaufnjeri <kamaufnjeri2019@gmail..com>
2024-03-11 16:23:22 +0300

    show files by id, pagination ans showing all files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -120,33 +120,33 @@ class FilesController {
 
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
 
-    const parentId = req.query.parentId && req.query.parentId !== '0'? ObjectId(req.query.parentId) : 0;
+    const parentId = req.query.parentId && req.query.parentId !== '0' ? ObjectId(req.query.parentId) : 0;
 
     const filesCount = await mongoClient.filesCollection.countDocuments({
-        userId: ObjectId(userId),
-        parentId,
+      userId: ObjectId(userId),
+      parentId,
     });
 
-    if (parseInt(filesCount) === 0) return res.status(200).json([]);
-    
-    const page = req.query.page && req.query.page !== '0' ? parseInt(req.query.page) : 0;
+    if (parseInt(filesCount, 10) === 0) return res.status(200).json([]);
+
+    const page = req.query.page && req.query.page !== '0' ? parseInt(req.query.page, 10) : 0;
 
     const jump = page * 20;
 
     const files = await mongoClient.filesCollection.aggregate([
-        { '$match': { userId: ObjectId(userId), parentId } } ,
-        { '$skip': jump },
-        { '$limit': 20 },
+      { $match: { userId: ObjectId(userId), parentId } },
+      { $skip: jump },
+      { $limit: 20 },
 
     ]).toArray();
 
-    const filesJson = files.map(file => ({
-        id: file._id,
-        userId: file.userId,
-        name: file.name,
-        type: file.type,
-        isPublic: file.isPublic,
-        parentId: file.parentId
+    const filesJson = files.map((file) => ({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
     }));
 
     return res.status(200).json(filesJson);
